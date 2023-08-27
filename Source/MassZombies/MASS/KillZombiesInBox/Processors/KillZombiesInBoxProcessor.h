57176dde344cf1c4a486db727c717ac406f0f19f
Can Göktaş <goktascan1@gmail.com>
2023-08-27 10:50:11 +0300

    naming corrections, removed hololens folder

--- Source/MassZombies/MASS/KillZombiesInBox/Processors/KillZombiesInBoxProcessor.h
+++ Source/MassZombies/MASS/KillZombiesInBox/Processors/KillZombiesInBoxProcessor.h
@@ -7,7 +7,9 @@
 #include "KillZombiesInBoxProcessor.generated.h"
 
 /**
- * 
+ * UKillZombiesInBoxProcessor
+ *
+ * Kills the zombies when they are in a box around player.
  */
 UCLASS()
 class MASSZOMBIES_API UKillZombiesInBoxProcessor : public UMassProcessor
